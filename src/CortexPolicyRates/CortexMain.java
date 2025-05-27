2a064cb229195fa31de68e8c27c42b81e7b44234
AnilK6259 <anilkutikuppala@gmail.com>
2025-05-27 09:16:37 +0530

    This is coomit from workspace1

--- src/CortexPolicyRates/CortexMain.java
+++ src/CortexPolicyRates/CortexMain.java
@@ -10,6 +10,7 @@ public class CortexMain {
 
 		rdg.calculateRates("20221212ARG----/DA/UALL/SCALL/OY/012M/999K/A/Mo096/Mi042/Mo---Mi---/VR06/£000/FTD/VU-/CC-/VCC/PP-/LB80/CA---/TNA/WD---/DO---/FO-/	","ARG.63828400.02.01	", "16-01-2024","NA");
 
+		System.out.println("newChange");
 	}
 
 }
